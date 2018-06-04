f171ad5375ab081f5c0e506214a41113bafe22c1
dantenerolmh <dantenerolmh@yahoo.com>
2018-06-04 12:53:33 +0800

    declare struct Color and add private members; copy external configuration file game.cfg into project folder

--- assignment_01/Solution/ConfigurationParser/ConfigParser.h
+++ assignment_01/Solution/ConfigurationParser/ConfigParser.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <string>;
+
+
 class ConfigParser
 {
 public:
 	ConfigParser();
 	~ConfigParser();
+	
+	struct Color
+	{
+		float r;
+		float g;
+		float b;
+	};
+
+private:
+	float spinning;
+	float spinSpeed;
+
+	Color backgroundColor;
+	std::string terrainPath;
+
+	float terrainWidth;
+	float terrainDepth;
+	float terrainHeight;
 };
 
